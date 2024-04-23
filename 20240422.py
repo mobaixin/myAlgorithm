be180b05e8dd6068e7cbfcc3438c7279316e6b0c
”mobaixin“ <13687162414@163.com>
2024-04-23 12:13:27 +0800

    Add: 20240423-maxSatisfied

--- 20240422.py
+++ 20240422.py
@@ -20,3 +20,26 @@ class Solution:
         for i in range(1, target + 1):
             f[i] = sum(f[i - x] for x in nums if x <= i)
         return f[target]
+    
+    # 20240423
+    # 1052.爱生气的书店老板
+    def maxSatisfied(self, customers: list[int], grumpy: list[int], minutes: int) -> int:
+        # 时间复杂度 O(n)
+        # 空间复杂度 O(1)
+        n = len(customers)
+        if n <= minutes:
+            return sum(customers)
+        ans = 0
+        for i in range(n):
+            if grumpy[i] == 0:
+                ans += customers[i]
+                customers[i] = 0
+        
+        addNum = sum(customers[0:minutes])
+        maxAddNum = addNum
+        for i in range(1, n - minutes + 1):
+            addNum = addNum - customers[i - 1] + customers[i + minutes - 1]
+            maxAddNum = max(maxAddNum, addNum)
+
+        ans += maxAddNum
+        return ans

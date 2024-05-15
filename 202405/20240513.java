2d5d28b45feb96eb561540859dd51068b14f48e7
”mobaixin“ <13687162414@163.com>
2024-05-15 21:45:13 +0800

    Add: 20240515-2589-java-1

--- 202405/20240513.java
+++ 202405/20240513.java
@@ -1,3 +1,4 @@
+import java.util.Arrays;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -18,6 +19,36 @@ class Solution {
                 return -1;
             }
             ans += (c + 2) / 3;
+        }
+        return ans;
+    }
+
+    // 20240515
+    // 2589. 完成所有任务的最少时间
+    public int findMinimumTime(int[][] tasks) {
+        // 时间复杂度 O(nlogn + nU), U = max(endi)
+        // 空间复杂度 O(U)
+        int n = tasks.length;
+        Arrays.sort(tasks, (a, b) -> a[1] - b[1]);
+        int ans = 0;
+        int mx = tasks[n - 1][1];
+        boolean[] run = new boolean[mx + 1];
+        for (int[] t : tasks) {
+            int start = t[0];
+            int end = t[1];
+            int d = t[2];
+            for (int i = start; i <= end; i++) {
+                if (run[i]) {
+                    d--;    // 去掉运行中的时间点
+                }
+            }
+            for (int i = end; d > 0; i--) { // 剩余的 d 填充区间后缀
+                if (!run[i]) {
+                    run[i] = true;  // 运行
+                    d--;
+                    ans++;
+                }
+            }
         }
         return ans;
     }

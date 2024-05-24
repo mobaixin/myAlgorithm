bc4f4f9c28817bdef7ffa65d97c26eb2503d3560
caoliang11 <caoliang11@meituan.com>
2024-05-24 10:49:37 +0800

    Add: 20240522-2225-java-1

--- 202405/20240520.java
+++ 202405/20240520.java
@@ -1,7 +1,39 @@
-
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 // 20240520
 class Solution {
+    // 20240522
+    // 2225. 找出输掉零场或一场比赛的玩家
+    public List<List<Integer>> findWinners(int[][] matches) {
+        // 时间复杂度 O(nlogn)
+        // 空间复杂度 O(n)
+        int n = matches.length;
+        Map<Integer, Integer> lossCount = new HashMap<>();
+        for (int[] m : matches) {
+            if (!lossCount.containsKey(m[0])) {
+                lossCount.put(m[0], 0);
+            }
+            lossCount.merge(m[1], 1, Integer::sum);
+        }
+
+        List<List<Integer>> ans = Arrays.asList(new ArrayList<>(), new ArrayList<>());
+        for (Map.Entry<Integer, Integer> e : lossCount.entrySet()) {
+            int cnt = e.getValue();
+            if (cnt < 2) {
+                ans.get(cnt).add(e.getKey());
+            }
+        }
+
+        Collections.sort(ans.get(0));
+        Collections.sort(ans.get(1));
+        return ans;
+    }
+
     // 20240524
     // 1673. 找出最具竞争力的子序列s
     public int[] mostCompetitive(int[] nums, int k) {
